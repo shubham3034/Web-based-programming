68a8a67ba0b480ba297efb16120c8e902422341b
shubham3034 <101803743+shubham3034@users.noreply.github.com>
2022-04-21 20:42:26 +0530

    Add files via upload

---
+++ webpage.php
@@ -0,0 +1,11 @@
+<tml>
+<body bgcolor="skyblue" text="yellow">
+<?php
+echo "<h1><i><u><b>Home Page</h1></u><br>";
+echo "<h1><center>Tecnia institute of advanced studies</h1><center><br>";
+echo "<h2>There are two shifts in BCA department</h2><br>";
+echo "<h2>Shift 1 - 8:00 A.M</h2><br>";
+echo "<h2>Shift 2 - 11:10 A.M</h2><br>";
+?>
+</body>
+</html>
