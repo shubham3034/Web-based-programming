2596fa624a1f01d8c63343fc0a33762ca24bb075
Shubham <101803743+shubham3034@users.noreply.github.com>
2022-04-26 19:37:59 +0530

    Add files via upload

---
+++ SumVariable.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+echo "Variables are:<br>";
+$a = 10;
+$b = 20;
+$sum = $a+$b;
+echo "First variable is=".$a."<br>";
+echo "Second variable is=".$b."<br>";
+echo "Sum=".$sum."<br>";
+?>
+</body>
+</html>
