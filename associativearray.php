8e7a8d60f26979a497d597afd447a820f6037b66
Shubham <101803743+shubham3034@users.noreply.github.com>
2022-05-10 21:17:18 +0530

    Add files via upload

---
+++ associativearray.php
@@ -0,0 +1,11 @@
+<html>
+<?php
+$arr = array("hello=8","hey=9","10=class");
+echo "SHUBHAM<br>";
+foreach($arr as $y => $y_values)
+{
+	echo "key=".$y.",value=".$y_values;
+	echo "<br>";
+}
+?>
+</html>
